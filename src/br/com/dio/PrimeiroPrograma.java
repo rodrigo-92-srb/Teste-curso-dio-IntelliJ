f0c39adf7109c47c4a15f6410b4974a8d6487e47
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2022-01-23 01:34:28 -0300

    incluindo classe Cachorro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,5 +1,6 @@
 package br.com.dio;
 
+import br.com.dio.model.Cachorro;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
@@ -10,5 +11,10 @@ public class PrimeiroPrograma {
 
         Gato gato= new Gato();
         System.out.println(gato);
+
+        Cachorro cao1 = new Cachorro("Kika", "preto", 15);
+        Cachorro cao2 = new Cachorro("Dara", "marrom", 11);
+        System.out.println(cao1.getNome());
+        System.out.println(cao2.getNome());
     }
 }
---
+++ src/br/com/dio/model/Cachorro.java
@@ -0,0 +1,39 @@
+package br.com.dio.model;
+
+public class Cachorro {
+    private String nome;
+    private String cor;
+    private int idade;
+
+    public Cachorro() {}
+
+    public Cachorro(String nome, String cor, int idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
